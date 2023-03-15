0a8bc347308ceae7fdd68023bffd9cc44bf17ad2
A.S.Rawat <adityasrwt5@gmail.com>
2023-03-15 13:01:31 +0530

    Update

--- Doodle Jump/Assets/Scripts/platformSpawner.cs
+++ Doodle Jump/Assets/Scripts/platformSpawner.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 public class platformSpawner : MonoBehaviour
 {
     [SerializeField] GameObject platformPrefab;
-    [SerializeField] Transform wavePoints1;
-    [SerializeField] Transform wavePoints2;
-    [SerializeField] float minDelay = 0.1f;
-    [SerializeField] float maxDelay = 2f;
+    [SerializeField] int numberOfPlatforms;
+    [SerializeField] float levelWidth = 3f;
+    [SerializeField] float minY = 0.1f;
+    [SerializeField] float maxY = 2f;
     private void Start()
     {
-        StartCoroutine(spawner());
+        Vector3 spawnPos = new Vector3();
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPos.y += Random.Range(minY, maxY);
+            spawnPos.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformPrefab, spawnPos, Quaternion.identity);
+        }
+        //StartCoroutine(spawner());
     }
 
-    IEnumerator spawner()
-    {
-        while (true)
-        {
-            float Delay = Random.Range(minDelay, maxDelay);
-            float randomX = Random.Range(wavePoints1.position.x, wavePoints2.position.x);
-            Vector3 position = new Vector3(randomX, wavePoints1.position.y, wavePoints1.position.z);
-            yield return new WaitForSeconds(Delay);
-            GameObject clonePlatform = Instantiate(platformPrefab, position, Quaternion.identity);
-        }
-    }
 }

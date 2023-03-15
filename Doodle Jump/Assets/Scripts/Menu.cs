489514efc96c76fddbd094de589aa0ec8ec58535
A.S.Rawat <adityasrwt5@gmail.com>
2023-03-16 00:41:49 +0530

    Final Update

--- Doodle Jump/Assets/Scripts/Menu.cs
+++ Doodle Jump/Assets/Scripts/Menu.cs
@@ -5,6 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public void inusLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void nxtLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
     public void restartlvl()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
